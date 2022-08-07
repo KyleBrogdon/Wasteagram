907d989235a34f3e878ad4305c7fe129d02cceb5
rotorhead7700 <brogdonk@oregonstate.edu>
2022-08-07 12:48:36 -0500

    sample project code

---
+++ lib/entry_lists.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+
+class EntryLists extends StatefulWidget {
+  @override
+  _EntryListsState createState() => _EntryListsState();
+}
+
+// code citation: convert dateTime object to string
+// https://stackoverflow.com/questions/51579546/how-to-format-datetime-in-flutter
+class _EntryListsState extends State<EntryLists> {
+  DateFormat dateFormat = DateFormat('MMMM-dd-yyyy kk:mm');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Wastegram')),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasData && snapshot.data!.docs != null && snapshot.data!.docs.length > 0) {
+              return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    var post = snapshot.data!.docs[index];
+                    return ListTile(
+                        title: Text(dateFormat.format(post['submission_date'].toDate())),
+                        subtitle: Text(post['weight'].toString()));
+                  });
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          }),
+      floatingActionButton: NewEntryButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+/*
+ * As an example I have added functionality to add an entry to the collection
+ * if the button is pressed
+ */
+class NewEntryButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('posts')
+              .add({'submission_date': DateTime.now(), 'weight': 17});
+        });
+  }
+}
